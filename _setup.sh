7bc3a025b46dc13a8eba482e4db4ef0ce163f68d
Philipp Serr <episource@gmx.de>
2020-04-04 22:34:27 +0200

    Add filter setup script

---
+++ _setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+SCRIPT_DIR=$( dirname $( readlink -e $0 ) )
+git -C "$SCRIPT_DIR" config --local filter.plainzip.clean 'bash -c '"'"'T="$(mktemp)";cat - > "$T";N=$(ziptool "$T" get_num_entries u);N=${N//[^0-9]};C="";for ((i=0; i<N;i++)); do C="$C set_file_compression $i store u"; done; ziptool "$T" $C; cat "$T"; rm "$T"'"'"''
